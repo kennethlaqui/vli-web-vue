d02635b903ab7eb4eb49ef9935c332c7191ea7c6
Kenneth Laqui <kennethlaqui@yahoo.com>
2020-05-06 03:52:02 +0800

    Util Helper
    * export assignment for helper
    * static value

---
+++ src/util/helper.js
@@ -0,0 +1,81 @@
+// non-function
+let Vgender = [
+  {
+    value: 'M',
+    text: 'Male'
+  },
+  {
+    value: 'F',
+    text: 'Female'
+  },
+  {
+    value: 'O',
+    text: 'Others'
+  }
+]
+
+let Vcivilstatus = [
+  {
+    value: 'S',
+    text: 'Single'
+  },
+  {
+    value: 'M',
+    text: 'Married'
+  }
+]
+
+let Vratetype = [
+  {
+    value: 'M',
+    text: 'Monthly'
+  },
+  {
+    value: 'D',
+    text: 'Daily'
+  }
+]
+
+let Vweek = [
+  {
+    value: '1',
+    text: 'Monday'
+  },
+  {
+    value: '2',
+    text: 'Tuesday'
+  },
+  {
+    value: '3',
+    text: 'Wednesday'
+  },
+  {
+    value: '4',
+    text: 'Thursday'
+  },
+  {
+    value: '5',
+    text: 'Frvalueay'
+  },
+  {
+    value: '6',
+    text: 'Saturday'
+  },
+  {
+    value: '7',
+    text: 'Sunday'
+  }
+]
+
+let Vboolean = [
+  {
+    value: 'T',
+    text: 'Yes'
+  },
+  {
+    value: 'F',
+    text: 'No'
+  }
+]
+
+export { Vgender, Vcivilstatus, Vratetype, Vweek, Vboolean }
