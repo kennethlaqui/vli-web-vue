12985ae1fa154d4740259978acf04bda6c7802a7
Kenneth Laqui <kennethlaqui@yahoo.com>
2020-05-06 03:48:16 +0800

    Refactor dialog save and update component
    * Removed data, method and computed properties
    * Implement props only
    * Implement slots
    * Configurable

---
+++ src/components/dialogs/masterfile/DialogSaveUpdate.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    persistent
+    max-width="400"
+  >
+    <v-card>
+      <v-card-title>
+        <slot name="title">
+          {{ title }}
+        </slot>
+      </v-card-title>
+      <v-card-text>
+        <slot name="content">
+          {{ content }}
+        </slot>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <slot name="b-close">
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </slot>
+        <slot name="b-submit">
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Proceed
+          </v-btn>
+        </slot>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    },
+    content: {
+      type: String
+    },
+    dialog: {
+      type: Boolean,
+      default: false
+    },
+    dialogsave: Boolean
+  }
+}
+</script>
