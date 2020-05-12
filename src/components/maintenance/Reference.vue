ee34a4093b5bef8874b5892f13ed7960d6c24821
Kenneth Laqui <kennethlaqui@yahoo.com>
2020-05-12 23:03:43 +0800

    New added employment

--- src/components/maintenance/Reference.vue
+++ src/components/maintenance/Reference.vue
@@ -31,7 +31,7 @@
           v-show="settings.panelShow"
           class="mr-4"
           icon
-          @click="dialog.d_emplymnt = true, dialog.dialogTitle='Create Employment Status'"
+          @click="dialog.d_employment = true, dialog.dialogTitle='Create Employment Status'"
         >
           <v-icon>mdi-folder</v-icon>
         </v-btn>
@@ -50,7 +50,7 @@
         <v-expansion-panel-header>Employment</v-expansion-panel-header>
         <v-expansion-panel-content>
           <!-- Employment status component -->
-          <Employment></Employment>
+          <Employment :reloadEmployment="reloadEmployment"></Employment>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
@@ -153,32 +153,184 @@
         </v-btn>
       </template>
     </DialogReference>
-    <!-- employment -->
+    <!-- employment dialog -->
     <DialogReference
-      v-if="dialog.d_emplymnt"
+      v-if="dialog.d_employment"
       :title="dialog.dialogTitle"
       dialog
       width="800px"
     >
       <template v-slot:body>
         <v-row>
-          <v-col
-            cols="12"
-            md="4"
-          >
+          <!-- ID -->
+          <v-col cols="12" sm="6" md="3">
           <v-text-field
+            v-model="f_employment.cntrl_no"
+            label="ID"
+            hint="Must Unique and One Character Only."
+            persistent-hint
+            outlined
+            dense
+            rounded
           >
           </v-text-field>
           </v-col>
+          <!-- description -->
+          <v-col cols="12" sm="6" md="3">
+          <v-text-field
+            v-model="f_employment.descript"
+            label="Description"
+            outlined
+            dense
+            rounded
+          >
+          </v-text-field>
+          </v-col>
+          <!-- payroll group -->
+          <v-col cols="12" sm="6" md="3">
+            <v-select
+              v-model="f_employment.defu_paygroup"
+              :items="payrollGroup"
+              item-text="descript"
+              item-value="group_no"
+              label="Payroll Group"
+              outlined
+              dense
+              rounded
+            ></v-select>
+          </v-col>
+          <!-- rate type -->
+          <v-col cols="12" sm="6" md="3">
+            <v-select
+              v-model="f_employment.defu_rate_typ"
+              :items="rateType"
+              item-text="text"
+              item-value="value"
+              label="Rate Type"
+              outlined
+              dense
+              rounded
+            ></v-select>
+          </v-col>
         </v-row>
+        <!-- 2nd row -->
+        <v-row>
+          <!-- tax -->
+          <v-col cols="12" sm="6" md="3">
+            <v-select
+              v-model="f_employment.defu_comp_tax"
+              :items="trueOrFalse"
+              item-text="text"
+              item-value="value"
+              label="Tax"
+              hint="Compute"
+              persistent-hint
+              outlined
+              dense
+              rounded
+            ></v-select>
+          </v-col>
+          <!-- sss -->
+          <v-col cols="12" sm="6" md="3">
+            <v-select
+              v-model="f_employment.defu_comp_sss"
+              :items="trueOrFalse"
+              item-text="text"
+              item-value="value"
+              label="SSS"
+              hint="Compute"
+              persistent-hint
+              outlined
+              dense
+              rounded
+            ></v-select>
+          </v-col>
+          <!-- tax -->
+          <v-col cols="12" sm="6" md="3">
+            <v-select
+              v-model="f_employment.defu_comp_med"
+              :items="trueOrFalse"
+              item-text="text"
+              item-value="value"
+              label="Philhealth"
+              hint="Compute"
+              persistent-hint
+              outlined
+              dense
+              rounded
+            ></v-select>
+          </v-col>
+          <!-- pagibig -->
+          <v-col cols="12" sm="6" md="3">
+            <v-select
+              v-model="f_employment.defu_comp_pgi"
+              :items="trueOrFalse"
+              item-text="text"
+              item-value="value"
+              label="Pagibig"
+              hint="Compute"
+              persistent-hint
+              outlined
+              dense
+              rounded
+            ></v-select>
+          </v-col>
+        </v-row>
+        <!-- 3rd row -->
+        <v-row>
+          <!-- minimum wage -->
+          <v-col cols="12" sm="6" md="3">
+            <v-select
+              v-model="f_employment.defu_min_wage"
+              :items="trueOrFalse"
+              item-text="text"
+              item-value="value"
+              label="Minimum Wage"
+              outlined
+              dense
+              rounded
+            ></v-select>
+          </v-col>
+          <!-- disable -->
+          <v-col cols="12" sm="6" md="3">
+            <v-select
+              v-model="f_employment.disabled"
+              :items="trueOrFalse"
+              item-text="text"
+              item-value="value"
+              label="Disable"
+              outlined
+              dense
+              rounded
+            ></v-select>
+          </v-col>
+        </v-row>
+      </template>
+      <template v-slot:b-close>
+        <v-btn
+          color="green darken-1"
+          text
+          @click="dialog.d_employment = false"
+        >
+          Close
+        </v-btn>
+      </template>
+      <template v-slot:b-submit>
+        <v-btn
+          color="green darken-1"
+          text
+          @click="saveEmployment(), dialog.d_employment = false, reloadEmployment = false"
+        >
+          Save
+        </v-btn>
       </template>
     </DialogReference>
   </div>
 </template>
 <script>
 import axios from 'axios'
-import { Form } from 'vform'
-import { Vboolean } from '@/util/helper'
+// import { Form } from 'vform'
+import { Vboolean, Vratetype } from '@/util/helper'
 import Employment from '@/components/maintenance/reference/Employment'
 import Workstat from '@/components/maintenance/reference/Workstat'
 import DialogReference from '@/components/maintenance/dialog/Reference'
@@ -194,25 +346,43 @@ export default {
     return {
       primekey: localStorage.getItem('primekey'),
       f_workStatDefault: '',
+      f_employmentDefault: '',
       reloadWorkStat: false,
+      reloadEmployment: false,
+      rateType: [],
       trueOrFalse: [],
+      payrollGroup: [],
       dialog: {
         dialogTitle: '',
         d_workstat: false,
-        d_emplymnt: false
+        d_employment: false
       },
       settings: {
         panelIndex: -1,
         panelShow: false
       },
-      f_workStat: new Form({
+      f_workStat: {
         primekey: '',
         cntrl_no: '',
         descript: '',
         show_mst: 'F',
         disabled: 'F',
         w_dte_rsgn: 'F'
-      })
+      },
+      f_employment: {
+        primekey: '',
+        cntrl_no: '',
+        descript: '',
+        disabled: 'F',
+        defu_paygroup: '',
+        defu_rate_typ: 'M',
+        defu_comp_tax: 'T',
+        defu_comp_sss: 'T',
+        defu_comp_med: 'T',
+        defu_comp_pgi: 'T',
+        defu_min_wage: 'F'
+      }
+
     }
   },
   watch: {
@@ -221,35 +391,27 @@ export default {
     }
   },
   methods: {
-    loadPrimekeys () {
-      this.f_workStat.primekey = this.primekey
-    },
     setWorkStatDefault (value) {
-      const data = value
-      Object.keys(data).forEach(key => {
-        this.f_workStat[key] = data[key]
-      })
+      this.f_workStat = { ...value }
       this.f_workStat.primekey = this.primekey
       this.reloadWorkStat = true
     },
     setEmploymentDefault (value) {
-      const data = value
-      Object.keys(data).forEach(key => {
-        this.f_workStat[key] = data[key]
-      })
+      this.f_employment = { ...value }
+      this.f_employment.primekey = this.primekey
+      this.reloadEmployment = true
     },
     async saveWorkStat () {
       try {
         axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('access_token')
         if (this.$store.getters.loggedIn) {
           await new Promise((resolve, reject) => {
-            this.f_workStat.post('u/maintenance/reference/workstat/create')
+            axios.post('u/maintenance/reference/workstat/create', this.f_workStat)
               .then((response) => {
                 resolve(response)
                 this.snack = true
                 this.snackColor = 'success'
                 this.snackText = 'Successfully Saved'
-                this.reloadWorkStat = true
                 this.setWorkStatDefault(this.f_workStatDefault)
               })
               .catch(error => {
@@ -259,12 +421,45 @@ export default {
         }
       } catch (error) {
       }
+    },
+    async saveEmployment () {
+      try {
+        axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('access_token')
+        if (this.$store.getters.loggedIn) {
+          await new Promise((resolve, reject) => {
+            axios.post('u/maintenance/reference/employment/create', this.f_employment)
+              .then((response) => {
+                resolve(response)
+                this.snack = true
+                this.snackColor = 'success'
+                this.snackText = 'Successfully Saved'
+                this.setEmploymentDefault(this.f_employmentDefault)
+              })
+              .catch(error => {
+                reject(error)
+              })
+          })
+        }
+      } catch (error) {
+      }
+    },
+    loadPayrollGroup () {
+      this.$store.dispatch('retrievePayrollGroup', {
+        primekey: this.primekey
+      })
+        .then(response => {
+          this.payrollGroup = this.$store.getters.retrievePayrollGroup
+        })
     }
   },
   created () {
     this.f_workStatDefault = { ...this.f_workStat } // make a copy of work status empty object / default value
-    this.loadPrimekeys()
+    this.f_employmentDefault = { ...this.f_employment }
+    this.f_workStat.primekey = this.primekey
+    this.f_employment.primekey = this.primekey
     this.trueOrFalse = Vboolean
+    this.rateType = Vratetype
+    this.loadPayrollGroup()
   }
 }
 </script>
